70ddb5daf547c50087248ad86054478bd8e333c1
rokostik <rokostik@gmail.com>
2024-01-25 15:11:24 +0100

    changes from comments

--- example/src/index.ts
+++ example/src/index.ts
@@ -48,7 +48,7 @@ app.post("/", async (req: Request, res: Response) => {
   const result = await frcClient.verifyCaptchaResponse(frcCaptchaResponse);
   if (!result.wasAbleToVerify()) {
     // In this case we were not actually able to verify the response embedded in the form, but we may still want to accept it.
-    // It could mean there is a network issue or that the service is down. In those cases you generally want to accept submissions anyhow
+    // It could mean there is a network issue or that the service is down. In those cases you generally want to accept submissions anyhow.
     // That's why we use `shouldAccept()` below to actually accept or reject the form submission. It will return true in these cases.
 
     if (result.isClientError()) {
@@ -70,7 +70,7 @@ app.post("/", async (req: Request, res: Response) => {
   }
 
   // The captcha was OK, process the form.
-  formMessage; // Normally we would use the form data here and submit it to our database.
+  formMessage; // Normally we would use the form data in `formMessage` here and submit it to our database.
 
   res.render("index", {
     message: "✅ Your message has been submitted successfully.",
